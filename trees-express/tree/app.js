b8c493ecdab4c381a1e21f7ff697c845506ac6b8
antarid <tarand007@gmail.com>
2017-12-27 19:19:39 +0300

    sdf

--- trees-express/tree/app.js
+++ trees-express/tree/app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var trees = require('./routes/trees');
-var cities = require('./routes/cities');
-var heights = require('./routes/height');
+//var trees = require('./routes/trees');
+//var cities = require('./routes/cities');
+//var heights = require('./routes/height');
 
 var app = express();
 
@@ -27,9 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/trees', trees);
-app.use('/cities', cities);
-app.use('/heights', heights);
+//app.use('/trees', trees);
+//app.use('/cities', cities);
+//app.use('/heights', heights);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
